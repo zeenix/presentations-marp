bfc1e8746c8f84ae33e7839f6a46987961bc3a02
Zeeshan Ali Khan <zeeshanak@gnome.org>
2023-02-13 18:07:05 +0100

    Enable warnings from Rust samples

--- Rust/rust-for-beginners-training/src/lib.rs
+++ Rust/rust-for-beginners-training/src/lib.rs
@@ -1,3 +1,10 @@
+#![doc(test(attr(
+    allow(unused),
+    deny(warnings),
+    // W/o this, we seem to get some bogus warning about `extern crate zbus`.
+    allow(unused_extern_crates),
+)))]
+
 #[cfg(doctest)]
 mod doctests {
     doc_comment::doctest!("../01-dipping-toes.md");
